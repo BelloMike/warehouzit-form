7f187567bd2435a5f1c2b8fb2350879e52f042d9
BelloMike <povamaxplanet@gmail.com>
2023-08-03 13:21:03 +0100

    FIRST

--- app/page.js
+++ app/page.js
@@ -1,113 +1,293 @@
-import Image from 'next/image'
+"use client";
+
+import Head from 'next/head';
+import { useState } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+  const [showWelcomeMessage, setShowWelcomeMessage] = useState(true);
+  const [isLoginForm, setIsLoginForm] = useState(true);
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    fullName: '',
+    lastName: '',
+    phoneNumber: '',
+    confirmPassword: '',
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [id]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (isLoginForm) {
+      // Login form validation
+      if (!formData.email.trim()) {
+        alert('Please enter your email address');
+        return;
+      }
+      if (!formData.password.trim()) {
+        alert('Please enter your password');
+        return;
+      }
+
+      // API call for login
+      try {
+        const response = await axios.post(
+          'https://warehouzitserver.onrender.com/api/v1/auth/login',
+          formData
+        );
+        console.log('Login success:', response.data);
+      } catch (error) {
+        console.error('Login failed:', error.message);
+      }
+    } else {
+      // Register form validation
+      if (!formData.fullName.trim()) {
+        alert('Please enter your full name');
+        return;
+      }
+      if (!formData.lastName.trim()) {
+        alert('Please enter your last name');
+        return;
+      }
+      if (!formData.email.trim()) {
+        alert('Please enter your email address');
+        return;
+      }
+      if (!formData.phoneNumber.trim()) {
+        alert('Please enter your phone number');
+        return;
+      }
+      if (!formData.password.trim()) {
+        alert('Please choose a password');
+        return;
+      }
+      if (formData.password !== formData.confirmPassword) {
+        alert('Passwords do not match');
+        return;
+      }
+
+      // API call for registration
+      try {
+        const response = await axios.post(
+          'https://warehouzitserver.onrender.com/api/v1/auth/register',
+          formData
+        );
+        console.log('Registration success:', response.data);
+      } catch (error) {
+        console.error('Registration failed:', error.message);
+      }
+    }
+  };
+
+  const toggleForm = () => {
+    setIsLoginForm((prevIsLoginForm) => !prevIsLoginForm);
+  };
+
+  const toggleWelcomeMessage = () => {
+    setShowWelcomeMessage(false);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div>
+      <Head>
+        <title>Login/Register Form</title>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"
         />
+      </Head>
+      <div className="min-h-screen flex items-center justify-center bg-green-100">
+        {showWelcomeMessage && (
+          <div className="max-w-md w-full p-8 bg-white shadow-md rounded-md">
+            <div className="bg-green-800 px-4 py-2 mb-10 flex items-center justify-between">
+              <p className="text-white">Hello, Welcome</p>
+              <p
+                className="text-white px-4 py-2 rounded cursor-pointer"
+                onClick={toggleWelcomeMessage}
+              >
+                X
+              </p>
+            </div>
+            <div className="mb-4 flex justify-start items-center">
+              <span
+                className={`text-green-800 cursor-pointer ${
+                  isLoginForm ? 'font-bold' : ''
+                }`}
+                onClick={toggleForm}
+              >
+                Login
+              </span>
+              <span className="mx-2 text-green-400">|</span>
+              <span
+                className={`text-green-800 cursor-pointer ${
+                  !isLoginForm ? 'font-bold' : ''
+                }`}
+                onClick={toggleForm}
+              >
+                Register
+              </span>
+            </div>
+
+            <hr className="mb-4 border-t border-green-300" />
+
+            <form onSubmit={handleSubmit}>
+              {isLoginForm ? (
+                <fieldset>
+                  <div className="mb-4">
+                    <input
+                      type="email"
+                      id="email"
+                      required
+                      className="input w-full py-2 px-4"
+                      placeholder="Email Address"
+                      value={formData.email}
+                      onChange={handleChange}
+                    />
+                  </div>
+
+                  <div className="mb-4">
+                    <input
+                      type="password"
+                      id="password"
+                      required
+                      className="input w-full py-2 px-4"
+                      placeholder="Password"
+                      value={formData.password}
+                      onChange={handleChange}
+                    />
+                  </div>
+
+                  <div className="mb-4 flex items-center justify-between">
+                    <label htmlFor="rememberMe" className="text-green-700">
+                      <input
+                        type="checkbox"
+                        id="rememberMe"
+                        className="mr-2 leading-tight"
+                      />
+                      Remember me
+                    </label>
+                    <a href="#" className="text-green-700">
+                      Forget password?
+                    </a>
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="button w-full bg-green-800 text-white py-2 px-4 rounded hover:bg-green-700 focus:outline-none"
+                  >
+                    Login
+                  </button>
+                </fieldset>
+              ) : (
+                <fieldset>
+                  <div className="mb-4">
+                    <input
+                      type="text"
+                      id="fullName"
+                      required
+                      className="input w-full py-2 px-4"
+                      placeholder="Full Name"
+                      value={formData.fullName}
+                      onChange={handleChange}
+                    />
+                  </div>
+
+                  <div className="mb-4">
+                    <input
+                      type="text"
+                      id="lastName"
+                      required
+                      className="input w-full py-2 px-4"
+                      placeholder="Last Name"
+                      value={formData.lastName}
+                      onChange={handleChange}
+                    />
+                  </div>
+
+                  <div className="mb-4">
+                    <input
+                      type="email"
+                      id="email"
+                      required
+                      className="input w-full py-2 px-4"
+                      placeholder="Email Address"
+                      value={formData.email}
+                      onChange={handleChange}
+                    />
+                  </div>
+
+                  <div className="mb-4">
+                    <input
+                      type="tel"
+                      id="phoneNumber"
+                      required
+                      className="input w-full py-2 px-4"
+                      placeholder="Phone Number"
+                      value={formData.phoneNumber}
+                      onChange={handleChange}
+                    />
+                  </div>
+
+                  <div className="mb-4">
+                    <input
+                      type="password"
+                      id="password"
+                      required
+                      className="input w-full py-2 px-4"
+                      placeholder="Password"
+                      value={formData.password}
+                      onChange={handleChange}
+                    />
+                  </div>
+
+                  <div className="mb-4">
+                    <input
+                      type="password"
+                      id="confirmPassword"
+                      required
+                      className="input w-full py-2 px-4"
+                      placeholder="Confirm Password"
+                      value={formData.confirmPassword}
+                      onChange={handleChange}
+                    />
+                  </div>
+
+                  <div className="mb-4 flex items-center justify-between">
+                    <label htmlFor="rememberMe" className="text-green-700">
+                      <input
+                        type="checkbox"
+                        id="rememberMe"
+                        className="mr-2 leading-tight"
+                      />
+                      Remember me
+                    </label>
+                    <a href="#" className="text-green-700">
+                      Forget password?
+                    </a>
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="button w-full bg-green-800 text-white py-2 px-4 rounded hover:bg-green-700 focus:outline-none"
+                  >
+                    Register
+                  </button>
+                </fieldset>
+              )}
+            </form>
+
+            <hr className="my-4 border-t border-green-300 mx-auto w-1/2" />
+
+          </div>
+        )}
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
